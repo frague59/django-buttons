300c4e40cb3ec2a608be901bf04b767a25de3323
GUERIN François <fguerin@ville-tourcoing.fr>
2017-02-14 13:15:33 +0100

    Updates buttons

--- buttons/templatetags/buttons_tags.py
+++ buttons/templatetags/buttons_tags.py
@@ -31,6 +31,10 @@ class IconPosition(enum.Enum):
     NONE = 'NONE'
 
 
+def _get_param(key, context, kwargs, default=None):
+    return context.get(key) or kwargs.get(key, None) or default
+
+
 @register.inclusion_tag('buttons/button.html', takes_context=True)
 def btn_button(context, **kwargs):
     """
@@ -41,7 +45,7 @@ def btn_button(context, **kwargs):
 
     + `text`: Button text, default 'Button'
     + `url`: Target URL, if needed
-    + `icon`: Button icon, default ``None``
+    + `icon`: Button icon, default ``None``, from `FontAwesome <http://fontawesome.io/icons/>`_
     + `icon_position`: Button icon position, , default ``None``, aka no icon displayed
     + `btn_css_class`: Button bootstrap class
     + `btn_id`: Button Id
@@ -56,27 +60,31 @@ def btn_button(context, **kwargs):
     logger.debug('btn_button() context = %s', context)
     logger.debug('btn_button() kwargs = %s', kwargs)
 
-    text = kwargs.pop('text', None) or context.get('text')
-    url = kwargs.pop('url', None) or context.get('url')
-    btn_id = kwargs.pop('id', None) or context.get('id') or kwargs.pop('btn_id', None) or context.get('btn_id')
+    text = _get_param('text', context, kwargs)
+    url = _get_param('url', context, kwargs)
+    btn_id = _get_param('id', context, kwargs) or _get_param('btn_id', context, kwargs)
 
-    tooltip = kwargs.pop('tooltip', None) or context.get('tooltip')
+    tooltip = _get_param('tooltip', context, kwargs)
 
-    icon = kwargs.pop('icon', settings.BUTTONS_ICON) or context.get('icon')
-    icon_position = kwargs.pop('icon_position', None) or context.get('icon_position') or settings.BUTTONS_ICON_POSITION
-    icon_css_extra = (kwargs.pop('icon_css_extra', None) or
-                      context.get('icon_css_extra') or
-                      settings.BUTTONS_ICON_CSS_EXTRA)
+    icon = _get_param('icon', context, kwargs,settings.BUTTONS_ICON)
 
-    btn_css_color = kwargs.pop('btn_css_color', None) or context.get('btn_css_color') or settings.BUTTONS_BTN_CSS_COLOR
-    btn_css_extra = kwargs.pop('btn_css_extra', None) or context.get('btn_css_extra') or settings.BUTTONS_BTN_CSS_EXTRA
+    icon_position = _get_param('icon_position', context, kwargs, settings.BUTTONS_ICON_POSITION)
+    if isinstance(icon_position, IconPosition):
+        icon_position = icon_position.value
+    else:
+        icon_position = icon_position.upper()
 
-    data_dismiss = kwargs.pop('data_dismiss', None) or context.get('data_dismiss')
-    data_toggle = kwargs.pop('data_toggle', None) or context.get('data_toggle')
-    data_target = kwargs.pop('data_target', None) or context.get('data_target')
-    data_placement = kwargs.pop('data_placement', None) or context.get('data_placement')
+    icon_css_extra = _get_param('icon_position', context, kwargs, settings.BUTTONS_ICON_CSS_EXTRA)
 
-    href_target = kwargs.pop('href_target', None) or context.get('href_target')
+    btn_css_color = _get_param('btn_css_color', context, kwargs, settings.BUTTONS_BTN_CSS_COLOR)
+    btn_css_extra = _get_param('btn_css_extra', context, kwargs, settings.BUTTONS_BTN_CSS_EXTRA)
+
+    data_dismiss = _get_param('data_dismiss', context, kwargs)
+    data_toggle = _get_param('data_toggle', context, kwargs)
+    data_target = _get_param('data_target', context, kwargs)
+    data_placement = _get_param('data_placement', context, kwargs)
+
+    href_target = _get_param('href_target', context, kwargs)
 
     # Dict initialization
     output = {'text': text,
@@ -116,8 +124,8 @@ def btn_download(context, url, text=_('Download'), icon='download', icon_positio
 
     :param context: Context data
     :param url: **Mandatory** target url
-    :param text: Button text
-    :param icon: Button icon
+    :param text: Button text, default 'Download'
+    :param icon: Button icon, default `download <http://fontawesome.io/icon/download/>`
     :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
     :param kwargs: Additional keyword args in:
 
@@ -135,17 +143,17 @@ def btn_back(context, text=_('Back'), icon='chevron-left', icon_position=IconPos
     Displays a ``btn_back`` button
 
     :param context: Context data
-    :param text: Button text
-    :param icon: Button icon
+    :param text: Button text, default 'Back'
+    :param icon: Button icon, default `chevron-left <http://fontawesome.io/icon/chevron-left/>`_
     :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.LEFT`
-    :param btn_css_color: Button css class, default `btn-primary`
+    :param btn_css_color: Button css class, default ``btn-primary``
     :param kwargs: Additional keyword args in:
 
     + `btn_css_class`: Button bootstrap class
 
     :returns: Render-able dict
     """
-    return btn_button(context, text=text, icon=icon, icon_position=icon_position, btn_css_color='btn-primary',
+    return btn_button(context, text=text, icon=icon, icon_position=icon_position, btn_css_color=btn_css_color,
                       url='javascript:history.btn_back()', **kwargs)
 
 
@@ -158,7 +166,7 @@ def btn_link(context, url, text=_('Link'), icon='btn_link', icon_position=IconPo
     :param context: Context data
     :param url: **Mandatory** target url
     :param text: link text, default 'link'
-    :param icon: Icon label, default 'link'
+    :param icon: Icon label, default `link <http://fontawesome.io/icon/link/>`_
     :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.LEFT`
     :param btn_css_color: Button bootstrap class, default 'btn-default'
 
@@ -179,7 +187,7 @@ def btn_home(context, url='/', text=_('Home'), icon='home', icon_position=IconPo
     :param context: Context data
     :param url: Target URL, default '/'
     :param text: Button text, default 'Home'
-    :param icon: Button icon, default 'fa-home'
+    :param icon: Button icon, default `home <http://fontawesome.io/icon/home/>`_
     :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.LEFT`
     :param btn_css_color: Button bootstrap class, default 'btn-primary'
     :param kwargs: Additional keyword args
@@ -198,7 +206,7 @@ def btn_submit(context, text=_('Submit'), icon='check', icon_position=IconPositi
 
     :param context: Context data
     :param text: Button text, default 'Submit'
-    :param icon: Button icon, default 'check'
+    :param icon: Button icon, default `check <http://fontawesome.io/icon/check/>`_
     :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
     :param btn_css_color: Base button color
     :param kwargs: Additional keyword args
@@ -218,8 +226,8 @@ def btn_list(context, url, text=_('List'), icon='list', icon_position=IconPositi
     :param context: Context data
     :param url: **Mandatory** target url
     :param text: Button text, default 'Submit'
-    :param icon: Button icon, default 'check'
-    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.LEFT`
+    :param icon: Button icon, default `list <http://fontawesome.io/icon/list/>`
+    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
     :param btn_css_color: Base button color
     :param kwargs: Additional keyword args
 
@@ -230,15 +238,15 @@ def btn_list(context, url, text=_('List'), icon='list', icon_position=IconPositi
 
 
 @register.inclusion_tag('buttons/button.html', takes_context=True)
-def btn_detail(context, url, text=_('Detail'), icon='detail', icon_position=IconPosition.RIGHT,
+def btn_detail(context, url, text=_('Detail'), icon='info', icon_position=IconPosition.RIGHT,
                btn_css_color='btn-primary', **kwargs):
     """
-    Displays a ``detail`` button
+    Displays a `Detail` button
 
     :param context: Context data
     :param url: **Mandatory** target url
     :param text: Button text, default 'Submit'
-    :param icon: Button icon, default 'check'
+    :param icon: Button icon, default `info <http://fontawesome.io/icon/info/>`_
     :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.LEFT`
     :param btn_css_color: Base button color
     :param kwargs: Additional keyword args
@@ -253,13 +261,14 @@ def btn_detail(context, url, text=_('Detail'), icon='detail', icon_position=Icon
 def btn_search(context, url, text=_('Search'), icon='search', icon_position=IconPosition.RIGHT,
                btn_css_color='btn-default', **kwargs):
     """
+    Renders a `Search` button
 
     :param context: Context data
     :param url: **Mandatory** target url
-    :param text: Button text, default 'Submit'
-    :param icon: Button icon, default 'check'
-    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.LEFT`
-    :param btn_css_color: Base button color
+    :param text: Button text, default 'Search'
+    :param icon: Button icon, default `search <http://fontawesome.io/icon/search/>`_
+    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
+    :param btn_css_color: Base button color, default `btn-default`
     :param kwargs: Additional keyword args
 
     :returns: Render-able dict
@@ -267,11 +276,101 @@ def btn_search(context, url, text=_('Search'), icon='search', icon_position=Icon
     return btn_button(url=url, text=text, icon=icon, icon_position=icon_position, btn_css_color=btn_css_color, **kwargs)
 
 
-@register.inclusion_tag('buttons/button.html')
-def btn_close(btn_css_color='btn-warning'):
-    return btn_button(btn_css_color=btn_css_color)
+@register.inclusion_tag('buttons/button.html', takes_context=True)
+def btn_close(context, text, icon='times', icon_position=IconPosition.RIGHT, btn_css_color='btn-warning',
+              data_dismiss=True, **kwargs):
+    """
+    Renders a `Close` button
+
+    :param context: Context data
+    :param text: Button text, default 'Search'
+    :param icon: Button icon, default `times <http://fontawesome.io/icon/times/>`_
+    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
+    :param btn_css_color: Base button color, default `btn-waning`
+    :param data_dismiss: Adds ``data-dismiss`` with `True` value
+    :param kwargs: Additional keyword args
+
+    :returns: Render-able dict
+    """
+    return btn_button(context, text=text, icon=icon, icon_position=icon_position, btn_css_color=btn_css_color,
+                      data_dismiss=data_dismiss, **kwargs)
 
 
-@register.inclusion_tag('buttons/button.html')
-def btn_login(url, text=_('Login'), icon='login', **kwargs):
-    return btn_button(url=url, text=text, icon=icon, **kwargs)
+@register.inclusion_tag('buttons/button.html', takes_context=True)
+def btn_login(context, url, text=_('Login'), icon='login', icon_position=IconPosition.RIGHT,
+              btn_css_color='btn-default', **kwargs):
+    """
+    Renders a ``Login`` button
+
+    :param context: Context data
+    :param url: **Mandatory** target url
+    :param text: Button text, default 'Login'
+    :param icon: Button icon, default `login <http://fontawesome.io/icon/login/>`_
+    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
+    :param btn_css_color: Base button color, default `btn-default`
+    :param kwargs: Additional keyword args
+
+    :returns: Render-able dict
+    """
+    return btn_button(context, url=url, text=text, icon=icon, icon_position=icon_position,
+                      btn_css_color=btn_css_color, **kwargs)
+
+
+@register.inclusion_tag('buttons/button.html', takes_context=True)
+def btn_logout(context, url, text=_('Logout'), icon='logout', icon_position=IconPosition.RIGHT,
+              btn_css_color='btn-default', **kwargs):
+    """
+    Renders a ``Logout`` button
+
+    :param context: Context data
+    :param url: **Mandatory** target url
+    :param text: Button text, default 'Logout'
+    :param icon: Button icon, default `logout <http://fontawesome.io/icon/logout/>`_
+    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
+    :param btn_css_color: Base button color, default `btn-default`
+    :param kwargs: Additional keyword args
+
+    :returns: Render-able dict
+    """
+    return btn_button(context, url=url, text=text, icon=icon, icon_position=icon_position,
+                      btn_css_color=btn_css_color, **kwargs)
+
+
+@register.inclusion_tag('buttons/button.html', takes_context=True)
+def btn_update(context, url, text=_('Update'), icon='pencil', icon_position=IconPosition.RIGHT,
+               btn_css_color='btn-warning', **kwargs):
+    """
+    Renders a ``Update`` button
+
+    :param context: Context data
+    :param url: **Mandatory** target url
+    :param text: Button text, default 'Update'
+    :param icon: Button icon, default `pencil <http://fontawesome.io/icon/pencil/>`_
+    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
+    :param btn_css_color: Base button color, default `btn-default`
+    :param kwargs: Additional keyword args
+
+    :returns: Render-able dict
+    """
+    return btn_button(context, url=url, text=text, icon=icon, icon_position=icon_position,
+                      btn_css_color=btn_css_color, **kwargs)
+
+
+@register.inclusion_tag('buttons/button.html', takes_context=True)
+def btn_delete(context, url, text, icon='trash', icon_position=IconPosition.RIGHT, btn_css_color='btn-danger',
+               **kwargs):
+    """
+    Renders a ``Delete`` button
+
+    :param context: Context data
+    :param url: **Mandatory** target url
+    :param text: Button text, default 'Delete'
+    :param icon: Button icon, default `trash <http://fontawesome.io/icon/trash/>`_
+    :param icon_position: Button icon position, default :attr:`buttons.templatetags.buttons_tags.IconPosition.RIGHT`
+    :param btn_css_color: Base button color, default `btn-danger`
+    :param kwargs: Additional keyword args
+
+    :returns: Render-able dict
+    """
+    return btn_button(context, url=url, text=text, icon=icon, icon_position=icon_position,
+                      btn_css_color=btn_css_color, **kwargs)
