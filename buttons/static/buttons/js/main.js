b935be16dc3063338bf695507c63321c8ba2e2d3
GUERIN François <fguerin@ville-tourcoing.fr>
2017-10-12 16:20:31 +0200

    #57 Mode brouillon pour les manifestations + Creates a generic switch button

--- buttons/static/buttons/js/main.js
+++ buttons/static/buttons/js/main.js
@@ -1,21 +1,22 @@
 /** Functions for button */
 
-if (typeof window.SwitchButton === 'undefined') {
-    window.SwitchButton = function() {};
+if (typeof window.switchButtons === 'undefined') {
+    window.switchButtons = function() {};
 
-    window.SwitchButton.getSwitchFunctionName = function ($btn) {
+    window.switchButtons.getSwitchFunctionName = function ($btn) {
         var id = $btn.attr('id');
-        return id.replace(/-/g, '_') + '_SWITCH';
+        return id.replace(/-/g, '_').toUpperCase() + '_SWITCH';
     };
 
-    window.SwitchButton.changeSwitchDisplay = function ($btn, value, colors, icons, alts) {
+    window.switchButtons.changeSwitchDisplay = function ($btn, value, colors, icons, alts) {
+        $btn.data('value', value);
         if (value) {
             $btn.removeClass('text-' + colors[0]).addClass('text-' + colors[1]);
-            $btn.find('span.sr-only').html(alts[1]);
+            $btn.find('span.switch-title').html(alts[1]);
             $btn.find('.fa-' + icons[0]).removeClass('.fa-' + icons[0]).addClass('fa-' + icons[1]);
         } else {
             $btn.removeClass('text-' + colors[1]).addClass('text-' + colors[0]);
-            $btn.find('span.sr-only').html(alts[0]);
+            $btn.find('span.switch-title').html(alts[0]);
             $btn.find('.fa-' + icons[1]).removeClass('.fa-' + icons[1]).addClass('fa-' + icons[0]);
         }
     };
