fb36784bc799a554b4e1008e3fd120876a525a22
GUERIN François <fguerin@ville-tourcoing.fr>
2017-10-12 17:16:06 +0200

    #57 Mode brouillon pour les manifestations + Creates a generic switch button

--- buttons/static/buttons/js/main.js
+++ buttons/static/buttons/js/main.js
@@ -1,7 +1,12 @@
 /** Functions for button */
+const DEBUG_SWITCH = true;
+
 
 if (typeof window.switchButtons === 'undefined') {
-    window.switchButtons = function() {};
+
+    // Creates a namespace where lives the switchButtons
+    window.switchButtons = function () {
+    };
 
     window.switchButtons.getSwitchFunctionName = function ($btn) {
         var id = $btn.attr('id');
@@ -9,15 +14,29 @@ if (typeof window.switchButtons === 'undefined') {
     };
 
     window.switchButtons.changeSwitchDisplay = function ($btn, value, colors, icons, alts) {
+        // Sets the data-value attribute to the new value
         $btn.data('value', value);
+        if (DEBUG_SWITCH) {
+            console.debug('switchButtons.changeSwitchDisplay() icons = "{0}"'.format(icons.join(',')));
+            console.debug('switchButtons.changeSwitchDisplay() colors = "{0}"'.format(colors.join(',')));
+            console.debug('switchButtons.changeSwitchDisplay() alts = "{0}"'.format(alts.join(',')));
+        }
+
+        // Changes the button display
         if (value) {
-            $btn.removeClass('text-' + colors[0]).addClass('text-' + colors[1]);
-            $btn.find('span.switch-title').html(alts[1]);
-            $btn.find('.fa-' + icons[0]).removeClass('.fa-' + icons[0]).addClass('fa-' + icons[1]);
+            // Change color
+            $btn.find('.switch-icon').removeClass('text-' + colors[0]).addClass('text-' + colors[1])
+            // Change icon
+                .find('.fa').removeClass('fa-' + icons[0]).addClass('fa-' + icons[1])
+            // Change title
+                .attr('title', alts[1]);
         } else {
-            $btn.removeClass('text-' + colors[1]).addClass('text-' + colors[0]);
-            $btn.find('span.switch-title').html(alts[0]);
-            $btn.find('.fa-' + icons[1]).removeClass('.fa-' + icons[1]).addClass('fa-' + icons[0]);
+            // Change color
+            $btn.find('.switch-icon').removeClass('text-' + colors[1]).addClass('text-' + colors[0])
+            // Change icon
+                .find('.fa').removeClass('fa-' + icons[1]).addClass('fa-' + icons[0])
+            // Change title
+                .attr('title', alts[0]);
         }
     };
 }
