6c8d1f97bc29de4ad842000adc695e5cd1eaa68e
GUERIN François <fguerin@ville-tourcoing.fr>
2017-10-20 11:29:01 +0200

    Update ci: - Adds creation of dict package

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ from distutils.core import setup
 setup(
     name='django-buttons',
     packages=['django-buttons', ],
-    version='0.1',
+    version='0.2',
     description='An application providing some template tags to add buttons to pages',
     author='François GUÉRIN',
     author_email='fguerin@ville-tourcoing.fr',
     url='https://github.com/frague59/django-buttons',
     download_url='https://github.com/frague59/django-buttons/tarball/0.1',
-    keywords=['django', 'django-fontawesome', 'django-appconf'],
-    classifiers=[], requires=['django']
+    keywords=['django'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+
+                 ],
+    requires=['django', 'django-fontawesome', 'django-appconf']
 )
