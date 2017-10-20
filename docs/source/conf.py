b4608390c4b0e71693dc388bf5b4c20d43dd6ddc
GUERIN François <fguerin@ville-tourcoing.fr>
2017-10-20 13:39:16 +0200

    Update ci: - Adds creation of docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('..'))
 from django.conf import settings
-settings.setup()
+settings.configure()
 
 # -- General configuration ------------------------------------------------
 
