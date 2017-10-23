7864c5d320bc325e0b12ec32bc32374d41bef0be
GUERIN François <fguerin@ville-tourcoing.fr>
2017-10-23 08:17:43 +0200

    #53 Mestions légales

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from distutils.core import setup
-
 from setuptools import find_packages
 
 setup(
     name='django-buttons',
-    packages=find_packages('.'),
     version='0.2',
     description='An application providing some template tags to add buttons to pages',
     author='François GUÉRIN',
     author_email='fguerin@ville-tourcoing.fr',
     url='https://github.com/frague59/django-buttons',
-    # download_url='https://gitlab.ville.tg/fguerin/',
+    license='MIT',
     keywords=['django', 'buttons', 'bootstrap3'],
     classifiers=['Development Status :: 4 - Beta',
                  'Framework :: Django',
@@ -20,10 +18,15 @@ setup(
                  'License :: OSI Approved :: MIT License',
                  ],
     install_requires=['django', 'django-fontawesome', 'django-appconf'],
+
+    # Source files
+    packages=find_packages('.'),
+
+    # Includes static files
     include_package_data=True,
-    packages_data={"./docs/": ["*.txt", "*.rst", "*.py", ],
-                   './static/': ['*.js', '*.less', '*.map', '*.css'],
-                   './templates/': ['*.html'],
-                   },
+    packages_data={"docs": ["*", ],
+                   'static': ["*", ],
+                   'templates': ["*", ]},
 
 )
+
