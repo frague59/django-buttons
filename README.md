126d23744a0de7dd85934c69430866b7af8dc7c6
François GUÉRIN <frague59@gmail.com>
2021-03-04 12:20:14 +0100

    Updates CI

---
+++ README.md
@@ -0,0 +1,43 @@
+# django-buttons
+
+This application provides some simple template tags to insert buttons into templates.
+
+## Installation
+
+Install application with pypi:
+
+```sh
+$ pip install django-buttons                # Does not installs any fontawesome -- Must be installed manually
+$ pip install django-buttons[fontawesome4]  # Installs django-fontawesome
+$ pip install django-buttons[fontawesome5]  # Installs django-fontawesome5
+```
+Add application to your INSTALLED_APPS:
+
+```python
+INSTALLED_APPS = [
+   ...
+   'buttons',
+   ...
+]
+
+BUTTONS_FONTAWESOME_VERSION = 4 # Use 5 ti use fontawesome-5 as icon library
+```
+
+## Use buttons in your templates
+
+```html
+ {% load buttons_tags %}
+ ...
+ {% btn_home %}
+```
+
+Buttons can have some parameters :
+
++ **url**: target url
++ **title**: displayed text
++ **icon**: fa aware name, ie. 'home' for
+  [fa-home](http://fontawesome.io/icon/home/)
++ **icon_position**: Position of the icon, 'right', 'left' or 'none'
+  (no icon displayed) ...
+
+**Enjoy !**
--- setup.py
+++ setup.py
@@ -1,35 +1,45 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 import sys
 import os
 from distutils.core import setup
 from setuptools import find_packages
 
 # Loads the version from package
-sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+sys.path.append(os.path.join(os.path.dirname(__file__), "."))
 from buttons import __version__ as version
 
 setup(
-    name='django-buttons',
+    name="django-buttons",
     version=version,
-    description='An application providing some template tags to add buttons to pages',
-    author='François GUÉRIN',
-    author_email='fguerin@ville-tourcoing.fr',
-    url='https://github.com/frague59/django-buttons',
-    license='MIT',
-    keywords=['django', 'buttons', 'bootstrap3'],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Framework :: Django',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: MIT License',
-                 ],
-    install_requires=['django', 'django-fontawesome', 'django-appconf'],
-
+    description="An application providing some template tags to add buttons to pages",
+    author="François GUÉRIN",
+    author_email="fguerin@ville-tourcoing.fr",
+    url="https://github.com/frague59/django-buttons",
+    license="MIT",
+    keywords=[
+        "django",
+        "buttons",
+        "bootstrap3",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "django",
+        "django-appconf",
+    ],
+    extra_requires={
+        "fontawesome4": [
+            "django-fontawesome",
+        ],
+        "fontawesome5": [
+            "django-fontawesome5",
+        ],
+    },
     # Source files
-    packages=find_packages('.'),
-
+    packages=find_packages("."),
     # Includes static files
     include_package_data=True,
-
 )
-
